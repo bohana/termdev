90f9b6cff8cd99023c29c80daa89462c4e22e114
Bruno Ohana <bohana@gmail.com>
2020-04-15 23:28:17 +0100

    Add support for starship and fancy fonts

--- bash/bash_aliases.sh
+++ bash/bash_aliases.sh
@@ -1,5 +1,6 @@
 alias vim=nvim
 alias gs="git status"
 alias ll="ls -ltr"
-alias sw="conda activate"
-
+alias sw="conda activate \$1 && unset PS1"
+alias psi="python setup.py install"
+alias pt="pytest -vv tests/"
--- deploy.sh
+++ deploy.sh
@@ -1,24 +1,48 @@
+function show_info()
+{
+  local msg="OK!"
+  if [ ! -z "$1" ]; then
+    msg="$1"
+  fi
+  echo -e "\033[0;36m${msg}\033[0m"
+}
+
 mkdir -p ~/.termdev
 mkdir -p ~/.config/nvim
 
-echo "[termdev] - copying bash helper scripts"
+show_info "[termdev] - copying bash helper scripts"
 cp -v bash/*sh ~/.termdev
 chmod 750 ~/.termdev/*sh
 
-echo "[termdev] - git setup"
+show_info "[termdev] - git setup"
 cp -v git/gitconfig ~/.gitconfig
 
-echo "[termdev] - nvim setup"
+show_info "[termdev] - nvim setup"
 cp -v nvim/init.vim ~/.config/nvim
 
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
         https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-echo "[termdev] - installing plugins"
+show_info "[termdev] - installing nvim plugins"
 nvim -c "PlugInstall|qa"
 
-echo "[termdev] - python packages"
+show_info "[termdev] - python packages"
 pip install -r python/requirements.txt
 
-echo "[termdev] - tmux"
+show_info "[termdev] - tmux"
 cp tmux/tmux.conf ~/.tmux.conf
+
+# fancy-looking fonts
+# https://github.com/powerline/fonts
+show_info "[termdev] - powerline fonts"
+ROOT=$PWD
+mkdir -p ~/tmp && cd ~/tmp
+git clone https://github.com/powerline/fonts.git --depth=1
+cd fonts
+./install.sh
+cd $ROOT
+
+show_info "[termdev] - starship prompt"
+curl -fsSL https://starship.rs/install.sh | bash
+mkdir -p ~/.config && touch ~/.config/starship.toml
+cp -v starship/starship.toml ~/.config/starship.toml
